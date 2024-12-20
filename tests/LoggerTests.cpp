c2f4e9151cc1be8051327c9d38ce82c3e0faa6cf
B33Boy <abhip202@gmail.com>
2024-12-20 00:26:50 -0500

    feat: create wrapper to capture standard output of a function

--- tests/LoggerTests.cpp
+++ tests/LoggerTests.cpp
@@ -5,6 +5,17 @@
 
 #include "logger.hpp"
 
+auto capture_output(std::function<void()> func) -> std::string
+{
+    std::stringstream ss;
+    std::streambuf* cout_buffer = std::cout.rdbuf(ss.rdbuf());
+
+    func();
+
+    std::cout.rdbuf(cout_buffer);
+    return ss.str();
+}
+
 namespace LoggerTests
 {
 
@@ -13,19 +24,14 @@ namespace LoggerTests
         // Arrange
         Logger::ConsoleLogger logger{};
 
-        std::stringstream ss;
-        std::streambuf* cout_buffer = std::cout.rdbuf(ss.rdbuf());
-
         std::string expected = "[INFO] This is a info message.\n";
 
         // Act
-        LOG(logger, Logger::Level::INFO, "This is a info message.");
-        std::string actual = ss.str();
+        std::string actual = capture_output([&logger]() {
+            LOG(logger, Logger::Level::INFO, "This is a info message.");
+            });
 
         // Assert
         EXPECT_EQ(expected, actual);
-
-        // Cleanup
-        std::cout.rdbuf(cout_buffer);
     }
 }
